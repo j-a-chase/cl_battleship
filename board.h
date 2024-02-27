e18498c1cc5794ae2bd4b54b8561df2068eb8e09
James Chase <gochases@gmail.com>
2024-02-26 20:31:18 -0700

    Add Board pure viritual class

--- board.h
+++ board.h
@@ -1,4 +1,14 @@
 #ifndef BOARD_H
 #define BOARD_H
 
+class Board {
+    private:
+        char board[10][10];
+
+    public:
+        virtual void printBoard() = 0;
+        virtual void updateTile(int row, int col) = 0;
+        virtual void resetBoard() = 0;
+};
+
 #endif
